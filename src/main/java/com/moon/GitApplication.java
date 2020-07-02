1d29ce3fdbdfac5f20d45c1f7bd1a1894cbee65f
zhangcheng <zhangcheng>
2020-07-02 13:58:18 +0800

    jenkins project test

---
+++ src/main/java/com/moon/GitApplication.java
@@ -0,0 +1,30 @@
+package com.moon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ProjectName: git-test
+ * @Package: com.moon
+ * @ClassName: GitApplication
+ * @Author: Administrator
+ * @Description:
+ * @Date: 2020-7-2 12:57
+ * @Version: 1.0
+ */
+@EnableAutoConfiguration
+@RestController
+public class GitApplication {
+
+    @RequestMapping("")
+    public String hello(){
+        return "HELLO GIT";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class,args);
+    }
+}
