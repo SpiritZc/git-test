ef47debd328a1a927a9a78dfb50468907c0acdb2
zhangcheng <zhangcheng>
2020-07-06 10:12:23 +0800

    v1

--- src/main/java/com/moon/GitApplication.java
+++ src/main/java/com/moon/GitApplication.java
@@ -3,6 +3,8 @@ package com.moon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,14 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @EnableAutoConfiguration
 @RestController
-public class GitApplication {
-
-    @RequestMapping("")
-    public String hello(){
-        return "HELLO GIT";
-    }
+public class GitApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GitApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GitApplication.class);
     }
 }
